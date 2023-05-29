e446ea91bb5777db4ea3151769f3182e26a47c0f
TonevS <tone0001@hz.nl>
2023-05-29 17:48:38 +0200

    Added inputs for monster type/race/attributes/level

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,72 @@ async function getYGOCardFromAPI(type) {
         console.log(error);
     }
 }
+// async function getYGOArchetypesFromAPI() {
+//     try {
+//         const response = await fetch("https://db.ygoprodeck.com/api/v7/archetypes.php");
+//         const archetypes = await response.json();
+//         return archetypes;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// const monsterArchetypeSelect = document.createElement('select');
+// options = [];
+// for (let i = 0; i < archetypes.length; i++) {
+//     options.push(document.createElement('option'));
+//     options[i].innerHTML = archetypes[i].archetype_name;
+//     options[i].value = archetypes[i].archetype_name;
+//     monsterArchetypeSelect.append(options[i]);
+// }
 
 function generateMonsterCardDOM() {
+
+    const monsterLevelSelect = document.createElement('select');
+    let options = [];
+    for (let i = 0; i < 12; i++) {
+        options.push(document.createElement('option'));
+        options[i].innerHTML = i + 1;
+        options[i].value = i + 1;
+        monsterLevelSelect.append(options[i]);
+    }
+
+    const monsterTypeSelect = document.createElement('select');
+    const types = ['Effect Monster', 'Flip Effect Monster', 'Flip Tuner Effect Monster', 'Gemini Monster', 'Normal Monster', 'Normal Tuner Monster', 'Pendulum Effect Monster', 'Pendulum Flip Effect Monster', 'Pendulum Normal Monster', 'Pendulum Tuner Effect Monster', 'Ritual Effect Monster', 'Ritual Monster', 'Spirit Monster', 'Toon Monster', 'Tuner Monster', 'Union Effect Monster', "Fusion Monster", 'Link Monster', 'Pendulum Effect Fusion Monster', 'Synchro Monster', 'Synchro Pendulum Effect Monster', 'Synchro Tuner Monster', 'XYZ Monster', 'XYZ Pendulum Effect Monster'];
+    options = [];
+    for (let i = 0; i < types.length; i++) {
+        options.push(document.createElement('option'));
+        options[i].innerHTML = types[i];
+        options[i].value = types[i];
+        monsterTypeSelect.append(options[i]);
+    }
+
+    const monsterRaceSelect = document.createElement('select');
+    const races = ['Aqua', 'Beast', 'Beast-Warrior', 'Creator-God', 'Cyberse', 'Dinosaur', 'Divine-Beast', 'Dragon', 'Fairy', 'Fiend', 'Fish', 'Insect', 'Machine', 'Plant', 'Psychic', 'Pyro', 'Reptile', 'Rock', 'Sea Serpent', 'Spellcaster', 'Thunder', 'Warrior', 'Winged Beast', 'Wyrm', 'Zombie'];
+    options = [];
+    for (let i = 0; i < races.length; i++) {
+        options.push(document.createElement('option'));
+        options[i].innerHTML = races[i];
+        options[i].value = races[i];
+        monsterRaceSelect.append(options[i]);
+    }
+
+    const monsterAttributeSelect = document.createElement('select');
+    const attributes = ['DARK', 'DIVINE', 'EARTH', 'FIRE', 'LIGHT', 'WATER', 'WIND'];
+    options = [];
+    for (let i = 0; i < attributes.length; i++) {
+        options.push(document.createElement('option'));
+        options[i].innerHTML = attributes[i];
+        options[i].value = attributes[i];
+        monsterAttributeSelect.append(options[i]);
+    }
+
     const getCardButton = document.createElement('button');
     getCardButton.textContent = 'Get Card';
 
     getCardButton.addEventListener('click', () => displayYGOCard('Normal Monster'));
 
     const inputsDiv = document.getElementById('inputs');
-    inputsDiv.append(getCardButton);
+    inputsDiv.append(monsterLevelSelect, monsterTypeSelect, monsterRaceSelect, monsterAttributeSelect, getCardButton);
 }
 
 async function displayYGOCard(type) {
